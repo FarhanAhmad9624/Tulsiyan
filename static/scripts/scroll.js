0c01cf3ac448e1613e5775fe0b4cef7dc46b212c
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2024-08-01 17:25:15 +0530

    lower rectangle

--- static/scripts/scroll.js
+++ static/scripts/scroll.js
@@ -1,9 +1,39 @@
 const slider = document.querySelector('#products-deal');
 
 slider.addEventListener('wheel', (evt) => {
+
     evt.preventDefault();
-    slider.scrollLeft += evt.deltaY*5;
+    slider.scrollLeft += evt.deltaY*7;
     slider.scrollIntoView = ({behavior: 'smooth', inline: 'start'})
 });
 
 // document.querySelectorAll('#products-deal-lists li').scrollIntoView
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {;
+    isDown = true;
+    slider.classList.add('active');
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseleave', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false;
+    slider.classList.remove('active');
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX); // Scroll speed
+    slider.scrollLeft = scrollLeft - walk;
+})
