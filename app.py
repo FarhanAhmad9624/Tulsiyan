62e1b2c343986d4b18e9a365c8f36e2e50d7a3b9
Farhan <164605246+FarhanAhmad9624@users.noreply.github.com>
2024-08-09 21:01:24 +0530

    user-profile

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def main_page():
 def login_page():
     return render_template('./login.html')
 
-# if __name__ == "__main__":
-#     app.run(debug=True)   
+if __name__ == "__main__":
+    app.run(debug=True)   
--- static/scripts/drop-down.js
+++ static/scripts/drop-down.js
@@ -1,13 +1,18 @@
 /* WHILE USING THE DROP-DOWN.JS SCRIPT. ALWAYS REMEMBER THAT THE DROP DOWN CONTAINER 
-IS IDNETIFIES BY IT'S LABEL WHICH SHOULD BE INSIDE THE <P> TAG. AND THE CLASSES OF CONTAINER
+IS IDNETIFIES BY IT'S LABEL WHICH can be any TAG. AND THE CLASSES OF CONTAINER
 AND OPTIONS SHOULD BE .option-name AND .options RESPECTIVELY*/
 
 
-const containerLabel = document.querySelectorAll('.drop-down p'); //container is identifying with its child p
-const containerArray = Array.from(containerLabel);  //converting cotainer elements into an array
+const container = document.querySelectorAll('.drop-down'); //container is identifying
+const containerArray = [];  //converting cotainer child elements into an array
 
-const dropDown = document.querySelectorAll('.drop-down');
-const dropDownArray = Array.from(dropDown);
+container.forEach(element => {
+    containerArray.push(element.firstElementChild);
+}
+)
+
+// const dropDown = document.querySelectorAll('.drop-down');
+// const dropDownArray = Array.from(dropDown);
 
 let options; 
 
@@ -16,11 +21,12 @@ document.addEventListener('click', (element) => {
     
     //checking whether the clicked element is a drop-down list or not
     for(let i = 0; i < containerArray.length; i++){
-
+        console.log(element.target);
         //selecting the options of the selected container
         let containerId = containerArray[i].getAttribute('id');
         options = document.querySelector(`#${containerId} + .options`);
         if(element.target == containerArray[i]){
+            console.log("yes");
             window.getComputedStyle(options).display == 'none'? display('block'): display('none')
         }
         else{
